68e32aa284acc4fb2ba3d13f10168a3c3be242fa
MichaelBlm <MichaelBlm94@gmail.com>
2023-04-11 11:48:08 -0700

    init commit to fix sequelize

---
+++ back-end/sequelize/models/task.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Task.init({
+    title: DataTypes.STRING,
+    dueDate: DataTypes.DATE,
+    description: DataTypes.STRING,
+    completed: DataTypes.BOOLEAN,
+    significant: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Task',
+  });
+  return Task;
+};
