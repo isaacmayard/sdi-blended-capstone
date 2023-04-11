c099b2e8a2b7088d640c0ce0a7c0a0050d71b58f
MichaelBlm <MichaelBlm94@gmail.com>
2023-04-11 11:50:01 -0700

    updated foreign keys

--- back-end/sequelize/migrations/20230411181652-create-msl.js
+++ back-end/sequelize/migrations/20230411181652-create-msl.js
@@ -1,34 +1,40 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Msls', {
+    await queryInterface.createTable("Msls", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "Users",
+          key: "id",
+          as: "userId",
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Msls');
-  }
+    await queryInterface.dropTable("Msls");
+  },
 };
