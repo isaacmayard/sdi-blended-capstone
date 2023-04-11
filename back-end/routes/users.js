b3acc9f9349a0cea1b169b1d1104b9f5b9cbb3b7
MichaelBlm <MichaelBlm94@gmail.com>
2023-04-11 11:53:02 -0700

    fixed sequelize database

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -4,9 +4,8 @@ const { User } = require("../sequelize/models");
 
 /* GET list of users */
 router.get("/", function (req, res, next) {
-  User.findAll().then((data) => {
-    res.status(200).send(data);
-  });
+  //FIX THE ISSUE OF SENDING DATA. FINDING BY PRIMARY KEY DOESN'T CRASH THE APP ANYMORE
+  db.User.findByPk(1).then((data) => res.status(200).send("SUCCESS"));
 });
 
 module.exports = router;
