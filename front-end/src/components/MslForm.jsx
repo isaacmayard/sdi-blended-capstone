d9439c692ca075b865bbfbfd3e577c15f766734c
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-04-12 17:57:44 -0400

    fix: refactor form

--- front-end/src/components/MslForm.jsx
+++ front-end/src/components/MslForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable no-nested-ternary */
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -9,26 +10,23 @@ export default function MslForm({ items }) {
     watch,
     formState: { errors },
   } = useForm();
-
   const onSubmit = (data) => console.log(data);
 
   return (
     <form className='tw-flex tw-max-w-xs tw-flex-col tw-items-center tw-rounded-xl tw-border-4 tw-bg-white dark:tw-bg-slate-800'>
-      {items?.map((item) =>
-        Object.keys(item).map(
-          (key) =>
-            key === 'id' || (
-              <div key={`${item.id}.${key}`} className='tw-flex tw-flex-col'>
-                <label>{key}</label>
-                <input
-                  type={
-                    key === 'email' ? 'text' : key === 'date' ? 'date' : 'text'
-                  }
-                  className='tw-border-2'
-                />
-              </div>
-            ),
-        ),
+      {items.map(
+        (item, index) =>
+          item === 'id' || (
+            <div key={index} className='tw-flex tw-flex-col'>
+              <label>{item}</label>
+              <input
+                type={
+                  item === 'email' ? 'text' : item === 'date' ? 'date' : 'text'
+                }
+                className='tw-border-2'
+              />
+            </div>
+          ),
       )}
       <button className='tw-rounded-sm tw-border-2' type='submit'>
         Submit
