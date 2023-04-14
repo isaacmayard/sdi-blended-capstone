cd6193621f413c709ab8efeec764d30b781e422a
alkumeidy <p.alkumeidy@gmail.com>
2023-04-14 13:33:22 -0600

    Updated faker data.

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -34,13 +34,15 @@ export default function NavBar() {
           <CDBSidebarMenuItem icon='user'>
             {users[0].rank} {users[0].firstName} {users[0].lastName}
             <ul>
-              <li>Userinfo </li>
+              <li className='tw-flex-wrap'>Unit: {users[0].unit} </li>
             </ul>
             <ul>
-              <li>Userinfo </li>
+              <li className='tw-overflow-scroll'>
+                Section: {users[0].work_section}{' '}
+              </li>
             </ul>
             <ul>
-              <li>Userinfo </li>
+              <li className='tw-flex-wrap'>{users[0].contact_number} </li>
             </ul>
           </CDBSidebarMenuItem>
           <CDBSidebarMenu>
