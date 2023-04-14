e55d8a8840c009a5eda3f9b7f70c587b2033ea0f
Angel De La Cruz <122130728+taos15@users.noreply.github.com>
2023-04-14 15:31:45 -0400

    fix: - added textarea for description (#19)
    fix: - refactor code
    
    fix: - changed tags on home to clear errors\n - changed backgound css to tw
    
    Feat: - Added msl form to the msl page

--- back-end/app.js
+++ back-end/app.js
@@ -1,30 +1,32 @@
-var express = require("express");
-var cors = require("cors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var authRouter = require("./routes/auth");
-var user_tasksRouter = require("./routes/user_tasks");
-var tasksRouter = require("./routes/tasks");
+var express = require('express');
 var cors = require('cors');
-const { Sequelize } = require("sequelize");
-require("dotenv").config({ path: "../.env" });
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var user_tasksRouter = require('./routes/user_tasks');
+var tasksRouter = require('./routes/tasks');
+const mslRouter = require('./routes/msl');
+var cors = require('cors');
+const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: '../.env' });
 
 var app = express();
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/auth", authRouter);
-app.use("/tasks", tasksRouter);
-app.use("/user_tasks", user_tasksRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/tasks', tasksRouter);
+app.use('/msl', mslRouter);
+app.use('/user_tasks', user_tasksRouter);
 
 module.exports = app;
