5e32b0a2b43e1dcd550b430584dea045a2f79ab1
IsaacMayard <isaacmayard@gmail.com>
2023-04-21 10:00:39 -0600

    I think this is it

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,7 +8,7 @@ import './style/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <div className=' tw-flex tw-min-h-[100vh] tw-w-screen tw-bg-gradient-to-r tw-from-sky-500 tw-from-5% tw-via-blue-800  tw-to-sky-500 tw-opacity-95'>
+    <div className=' tw-flex tw-min-h-[100vh] tw-w-screen tw-bg-gradient-to-r tw-from-sky-100 tw-from-5% tw-via-blue-700 tw-to-sky-800 tw-to-5% tw-opacity-95'>
       <App />
     </div>
   </Router>,
